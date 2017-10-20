4fab3e27f3a89f1d2866c40346f8d524c551c2c2
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-10-20 15:29:56 -0500

    Implemented basic site

---
+++ server/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var path_1 = require("path");
+var express = require("express");
+var clientPath = path_1.join(__dirname, '../client');
+var app = express();
+app.use(express.static(clientPath));
+console.log(process.env.GREETING);
+app.listen(3000, function () { return console.log('Listening on port 3000'); });
